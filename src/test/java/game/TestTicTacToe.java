138bc831488a11141795d750d6f2eacaa5922843
Paddy <pemcgovern@gmail.com>
2016-05-20 17:35:05 +0100

    Two new end-to-end tests.

--- src/test/java/game/TestTicTacToe.java
+++ src/test/java/game/TestTicTacToe.java
@@ -7,6 +7,39 @@ import org.junit.Test;
 public class TestTicTacToe {
 
     @Test
+    public void player2Wins() {
+        TicTacToe game = new TicTacToe();
+
+        game.playMove(1);
+        game.playMove(2);
+        game.playMove(3);
+        game.playMove(5);
+        game.playMove(4);
+        game.playMove(8);
+
+        MatcherAssert.assertThat(game.isOver(), Matchers.equalTo(true));
+        MatcherAssert.assertThat(game.result(), Matchers.equalTo(Result.P2_WIN));
+
+    }
+
+    @Test
+    public void player1Wins() {
+        TicTacToe game = new TicTacToe();
+
+        game.playMove(1);
+        game.playMove(2);
+        game.playMove(3);
+        game.playMove(4);
+        game.playMove(5);
+        game.playMove(6);
+        game.playMove(7);
+
+        MatcherAssert.assertThat(game.isOver(), Matchers.equalTo(true));
+        MatcherAssert.assertThat(game.result(), Matchers.equalTo(Result.P1_WIN));
+
+    }
+
+    @Test
     public void simpleTieGameWithoutInvalidMoves() {
         TicTacToe game = new TicTacToe();
 
@@ -24,4 +57,5 @@ public class TestTicTacToe {
         MatcherAssert.assertThat(game.result(), Matchers.equalTo(Result.TIE));
 
     }
+
 }
